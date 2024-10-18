fc6d5a7223a97469a5a911b5d1aab724c87ac130
b3smooth <bas43@kent.ac.uk>
2024-10-18 10:31:47 +0000

    V3 working

--- PartTwo.py
+++ PartTwo.py
@@ -1,22 +1,22 @@
 tf = False
 Utotal=0
 while tf==False:
-    Uval=int(input("please input your cash, 5,10,20,50 pence coins are accepted "))
+    Uval=input("please input your cash, 5,10,20,50 pence coins are accepted ")
     Sval=""
-    if (Uval.is_integer()):
+    if (type(Uval)==int):
         Uval=Uval
     else:
         for i in Uval:
-            if(i.is_integer):
-                Uval+=i
+            if(i=="1" or i=="2" or i=="3" or i=="4" or i=="5" or i=="6" or i=="7" or i=="8" or i=="9" or i=="0"):
+                Sval+=i
     
-    if(Uval==5 or Uval==10 or Uval==20 or Uval==50):
-        Utotal+=Uval
+    if(Sval=="5" or Sval=="10" or Sval=="20" or Sval=="50"):
+        Utotal+=int(Sval)
         print("total due="+str(75-Utotal))
         if(Utotal>=75):
             tf=True
-        else:
-            print("invalid currency")
+    else:
+        print("invalid currency")
     
 
 
