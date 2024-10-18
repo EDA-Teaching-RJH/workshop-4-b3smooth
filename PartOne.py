3dde3dccb535b62d8f69804bc00f8d2d11e2fef8
b3smooth <bas43@kent.ac.uk>
2024-10-18 09:57:46 +0000

    V1

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,9 @@
-
+Ustring= input("Please input a Camel Case variable name ")
+SUstring=""
+for i in Ustring:
+    if(i.isupper()== True):
+        temp= str(i.lower())
+        SUstring+=("_"+temp)
+    else:
+        SUstring+=i
+print(SUstring)
